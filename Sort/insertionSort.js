a0c3760954e8d0a7aedee253ce6e953101af37ed
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 17:37:06 -0500

    wrote out solution for insertion sort

--- Sort/insertionSort.js
+++ Sort/insertionSort.js
@@ -9,7 +9,14 @@
 
 const arr = [5, 3, 4, 1, 2];
 function insertionSort(arr) {
-
+  for(let  i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    // This works because of the var keyword allows access to j out of the scope of the for loop for j; 
+    for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1]  = currentVal;
+  }
   return arr;
 }
 
