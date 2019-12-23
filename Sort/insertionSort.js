2cf2eea589e00581e1ab28f5b3b50c6836271e82
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 17:18:31 -0500

    created insertion function

--- Sort/insertionSort.js
+++ Sort/insertionSort.js
@@ -1,2 +1,18 @@
 // Notes: 
-// Builds up the sort by graudually creating a larger left half which is always sorted
+// Builds up the sort by graudually creating a larger left portion which is always sorted
+
+
+// pick the second the element in the array 
+// Compare the one before it 
+// Continue to the next and ig 
+
+
+const arr = [5, 3, 4, 1, 2];
+function insertionSort(arr) {
+
+  return arr;
+}
+
+console.log(
+  insertionSort(arr)
+);
