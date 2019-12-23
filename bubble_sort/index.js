7ad42d99d1e281d251823d72b6d562178feb882f
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 11:42:04 -0500

    first commit

---
+++ bubble_sort/index.js
@@ -0,0 +1,30 @@
+// ______
+// Notes: 
+
+// Elementary Sorting Algorithms:
+
+// sorting methods have advantages and disadvantages in certian situatiions.
+// The built in sort method default uses the unicode to compare.
+
+
+// -----BUBBLE  SORT ----
+
+// Bubbule is a sorting algorithm where the largest values bubble up to the top on each iteration. 
+
+// Pseudocode
+
+// Create a for loop with i intialized going from the beginning to the end.
+// Create an inner for loop with j initialized at the beginning going to the end.
+// if arr[j]
+
+function bubbleSort(array) {
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++){
+      
+    }
+  }
+  return array;
+}
+console.log(bubbleSort(
+  [5, 3, 4, 1, 2])// [1, 2, 3, 4, 5]
+);
