692f406583f75840dec9801e06fe00e5ecddc964
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-23 20:59:46 -0500

    completed merge function

--- Sort/mergerSort.js
+++ Sort/mergerSort.js
@@ -2,7 +2,29 @@
 
 function merge(arr1, arr2) {
   let result = [];
-  
+  let arr1marker = 0; 
+  let arr2marker = 0; 
+
+  while(arr1marker !== arr1.length && arr2marker !== arr2.length){
+    if(arr1[arr1marker] < arr2[arr2marker]){
+      result.push(arr1[arr1marker]);
+      arr1marker++;
+    }
+    else{
+      result.push(arr2[arr2marker]);
+      arr2marker++;
+    }
+  }
+
+  if(arr1marker !== arr1.length){
+    result.push(...arr1.slice(arr1marker));
+  }
+
+  if(arr2marker !== arr2.length){
+    result.push(...arr2.slice(arr2marker));
+  }
+
+  return result; 
 }
 
 console.log(
