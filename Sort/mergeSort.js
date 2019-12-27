9a24da7bce90d8152dcf88d4674b795c0df0b1ee
Sayeed Joseph <sayeedjoseph@gmail.com>
2019-12-27 16:21:15 -0500

    reviewed mergeSort

--- Sort/mergeSort.js
+++ Sort/mergeSort.js
@@ -4,35 +4,97 @@
 // Time = O(n log n)
 // Space = O(n)
 
+
+function merge(left, right) { 
+  // Create an empty array to store sorted values
+  let sorted = [];
+  // Create a right and left pointer to iterate in while loop throguh left and right arrays to compare values
+  let pointerL = 0; 
+  let pointerR = 0; 
+
+  while( pointerL < left.length && pointerR < right.length) {
+    // Check which array has the smallest value to be stored in sorted
+    if(left[pointerL] < right[pointerR]){
+      sorted.push(left[pointerL]);
+      // increment pointer to compaare with value in the other array
+      pointerL++;
+    }
+    else { 
+      sorted.push(right[pointerR]);
+      pointerR;
+    }
+  }
+
+  // Check if either array has remaining values
+  if(pointerL !== left.length){
+    sorted.push(...left.slice(pointerL));
+  }
+
+  if(pointerR !== right.length){ 
+    sorted.push(...right.slice(pointerR));
+  }
+
+  
+  return sorted; 
+}
+
+function mergeSort(arr) {
+  if(arr.length <= 1) return arr; 
+  
+  let midIdx = Math.floor(arr.length / 2 );
+  let left = mergeSort(arr.slice(0, midIdx));
+  let right = mergeSort(arr.slice(midIdx));
+
+  return merge(left, right);
+}
+
+
+console.log(
+  mergeSort([10, 24, 76, 73, 72, 1, 9]),
+  mergeSort([10, 24, 76, 73])
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // --------MY ANSWER-----//
 // 
 
-function merge(arr1, arr2) {
-  let result = [];
-  let arr1marker = 0; 
-  let arr2marker = 0; 
+// function merge(arr1, arr2) {
+//   let result = [];
+//   let arr1marker = 0; 
+//   let arr2marker = 0; 
 
-  while(arr1marker !== arr1.length && arr2marker !== arr2.length){
-    if(arr1[arr1marker] < arr2[arr2marker]){
-      result.push(arr1[arr1marker]);
-      arr1marker++;
-    }
-    else{
-      result.push(arr2[arr2marker]);
-      arr2marker++;
-    }
-  }
+//   while(arr1marker !== arr1.length && arr2marker !== arr2.length){
+//     if(arr1[arr1marker] < arr2[arr2marker]){
+//       result.push(arr1[arr1marker]);
+//       arr1marker++;
+//     }
+//     else{
+//       result.push(arr2[arr2marker]);
+//       arr2marker++;
+//     }
+//   }
 
-  if(arr1marker !== arr1.length){
-    result.push(...arr1.slice(arr1marker));
-  }
+//   if(arr1marker !== arr1.length){
+//     result.push(...arr1.slice(arr1marker));
+//   }
 
-  if(arr2marker !== arr2.length){
-    result.push(...arr2.slice(arr2marker));
-  }
+//   if(arr2marker !== arr2.length){
+//     result.push(...arr2.slice(arr2marker));
+//   }
 
-  return result; 
-}
+//   return result; 
+// }
 
 // console.log(
 //   merge([1, 3, 5], [2, 4, 7]),
@@ -70,27 +132,20 @@ function merge(arr1, arr2) {
 //   return result;
 // }
 
-// console.log(
-//   merge([1, 3, 5], [2, 4, 7]),
-//   merge([1, 10, 50], [2, 14, 99, 100])
-// );
+// function mergeSort(arr) {
+//   // base case:
+//   if(arr.length <= 1) return arr; 
 
-//_________-
-
-function mergeSort(arr) {
-  // base case:
-  if(arr.length <= 1) return arr; 
-
-  // Findt he midpoing of the array
-  let midPoint = Math.floor(arr.length/ 2);
-  // Split arr into left and right
-  let left = mergeSort(arr.slice(0, midPoint));
-  let right = mergeSort(arr.slice(midPoint));
+//   // Findt he midpoing of the array
+//   let midPoint = Math.floor(arr.length/ 2);
+//   // Split arr into left and right
+//   let left = mergeSort(arr.slice(0, midPoint));
+//   let right = mergeSort(arr.slice(midPoint));
   
-  return merge(left, right);
-}
+//   return merge(left, right);
+// }
 
-console.log(
-  mergeSort([10, 24, 76, 73, 72, 1, 9]),
-  mergeSort([10, 24, 76, 73,])
-);
+// console.log(
+//   mergeSort([10, 24, 76, 73, 72, 1, 9]),
+//   mergeSort([10, 24, 76, 73,])
+// );
