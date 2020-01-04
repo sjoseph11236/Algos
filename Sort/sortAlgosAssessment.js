524e558fa8c435af7bd4d6225d24396d4aa0a490
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-04 17:17:52 -0500

    chore: fix digitCount function

--- Sort/sortAlgosAssessment.js
+++ Sort/sortAlgosAssessment.js
@@ -1,94 +1,94 @@
 // ---------------JAN 3.20 --------------------------------------------------------\\\
-// sort by moving the larger values to the end of the arr. 
-function bubbleSort(arr) {
-  // Create a variavle to update if there was a swap. 
-  let noSwap = true; 
-  // Create a for decrementing loop  for the number of times loop through the arr. 
-  for(let i = arr.length; i > 0 ; i--) { 
-  // Create a for loop that iterates through the arr
-    for (let j = 0; j < i - 1; j++) {
-      let current = arr[j];
-      let next = arr[j + 1];
-  // check if current element is larger than than next element
-      if(current > next) { 
-        // Swap 
-        let temp =  arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        noSwap = false;
-      }
-    }
-    if(noSwap) break;
-  }
+// // sort by moving the larger values to the end of the arr. 
+// function bubbleSort(arr) {
+//   // Create a variavle to update if there was a swap. 
+//   let noSwap = true; 
+//   // Create a for decrementing loop  for the number of times loop through the arr. 
+//   for(let i = arr.length; i > 0 ; i--) { 
+//   // Create a for loop that iterates through the arr
+//     for (let j = 0; j < i - 1; j++) {
+//       let current = arr[j];
+//       let next = arr[j + 1];
+//   // check if current element is larger than than next element
+//       if(current > next) { 
+//         // Swap 
+//         let temp =  arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//         noSwap = false;
+//       }
+//     }
+//     if(noSwap) break;
+//   }
 
-  // retunr arr. 
-  return arr;
-}
+//   // retunr arr. 
+//   return arr;
+// }
 
-console.log('BUBBLE ', 
-  bubbleSort(
-  [5, 3, 4, 1, 2])// [1, 2, 3, 4, 5]
-);
+// console.log('BUBBLE ', 
+//   bubbleSort(
+//   [5, 3, 4, 1, 2])// [1, 2, 3, 4, 5]
+// );
 
 //------Merge Sort-----//
 
-function merge(left, right) {
-  // create an empty arr. 
-  const sorted = [];
-  // create a left arr pointer  intially set to zero.
-  let leftIdx = 0; 
-  // create a right arr pointer intially set to zero.
-  let rightIdx = 0; 
-  // create a while loop that checks if bother pointer is not equal to theire respective arr.lengths. 
-  while(leftIdx !== left.length && rightIdx !== right.length){
-  // compare which element form each arr is less than and push to result arr. 
-    if(left[leftIdx] < right[rightIdx]) {
-      sorted.push(left[leftIdx]);
-      // Increment the left pointer 
-      leftIdx++;
-    }
-    else { 
-      sorted.push(right[rightIdx]);
-      rightIdx++;
-    }
-  }
-  // check if pointer is not equal to arr.length; 
-  if(leftIdx !== left.length) { 
-    let remainingLeft = left.slice(leftIdx);
-      // spread and push remaining values onto result arr
-    sorted.push(...remainingLeft);
-  }
+// function merge(left, right) {
+//   // create an empty arr. 
+//   const sorted = [];
+//   // create a left arr pointer  intially set to zero.
+//   let leftIdx = 0; 
+//   // create a right arr pointer intially set to zero.
+//   let rightIdx = 0; 
+//   // create a while loop that checks if bother pointer is not equal to theire respective arr.lengths. 
+//   while(leftIdx !== left.length && rightIdx !== right.length){
+//   // compare which element form each arr is less than and push to result arr. 
+//     if(left[leftIdx] < right[rightIdx]) {
+//       sorted.push(left[leftIdx]);
+//       // Increment the left pointer 
+//       leftIdx++;
+//     }
+//     else { 
+//       sorted.push(right[rightIdx]);
+//       rightIdx++;
+//     }
+//   }
+//   // check if pointer is not equal to arr.length; 
+//   if(leftIdx !== left.length) { 
+//     let remainingLeft = left.slice(leftIdx);
+//       // spread and push remaining values onto result arr
+//     sorted.push(...remainingLeft);
+//   }
 
-  if(rightIdx !== right.length) { 
-    let remainingRight = right.slice(rightIdx);
-      // spread and push remaining values onto result arr
-    sorted.push(...remainingRight);
-  }
+//   if(rightIdx !== right.length) { 
+//     let remainingRight = right.slice(rightIdx);
+//       // spread and push remaining values onto result arr
+//     sorted.push(...remainingRight);
+//   }
 
-  // return result arr. 
-  return sorted; 
-}
+//   // return result arr. 
+//   return sorted; 
+// }
 
-function mergeSort(arr){
-  // set the base case to check if  the length of the arr is less than or equal to 1; 
-  if(arr.length <= 1) return arr; 
-  // Find the midpoint of the by flooring half the arr.lenght; 
-  const midpoint = Math.floor(arr.length/ 2);
-  // create left arr with slice from the beginning of the arr  to the mid point.
-  // call the mergerSort functinon on left and right arr to create recursion. 
-  const left = mergeSort(arr.slice(0, midpoint));
-  // create a right arr with slice from midpoint to the end. 
-  // call the mergerSort functinon on left and right arr to create recursion. 
-  const right = mergeSort(arr.slice(midpoint));
-  // return merge envoked with the left and right as srgument. 
-  return merge(left, right);
-}
+// function mergeSort(arr){
+//   // set the base case to check if  the length of the arr is less than or equal to 1; 
+//   if(arr.length <= 1) return arr; 
+//   // Find the midpoint of the by flooring half the arr.lenght; 
+//   const midpoint = Math.floor(arr.length/ 2);
+//   // create left arr with slice from the beginning of the arr  to the mid point.
+//   // call the mergerSort functinon on left and right arr to create recursion. 
+//   const left = mergeSort(arr.slice(0, midpoint));
+//   // create a right arr with slice from midpoint to the end. 
+//   // call the mergerSort functinon on left and right arr to create recursion. 
+//   const right = mergeSort(arr.slice(midpoint));
+//   // return merge envoked with the left and right as srgument. 
+//   return merge(left, right);
+// }
 
-console.log(
-  'MERGE ',
-  mergeSort([10, 24, 76, 73, 72, 1, 9]),
-  mergeSort([10, 24, 76, 73])
-);
+// console.log(
+//   'MERGE ',
+//   mergeSort([10, 24, 76, 73, 72, 1, 9]),
+//   mergeSort([10, 24, 76, 73])
+// );
 
 //------Radix Sort-----//
 // helper function to determine what number is in certina place. 
@@ -99,8 +99,22 @@ function getDigit(num, place) {
 
 // helper function to determine the length of a digit. 
 function digitCount(num) { 
-
+  // Set a length variable to zero. 
+  let length = 0; 
+  // set a count vairavle to 1; 
+  let count = 1; 
+  // Create a while loop that  check if num is greater than count. 
+  while(count < Math.abs(num)) { 
+  // increase lenght by 1. 
+    length++;
+  // multiple count by 10. 
+    count * 10; 
+  }
+  // return length.
+  return length;
 }
+
+console.log('DIGIT COUNT ', digitCount(266781));//6
 
 // helper function to determine the length of the longest digit in the arr.  
 function mostDigits(arr) { 
@@ -131,37 +145,37 @@ console.log(
 );
 
 //------Selection Sort-----//
-// sort by moving the smallest value to front of on each pass.
-function selectionSort(arr) {
-  // create a for loop that iterates throgh the arr. 
-  for(let i = 0; i < arr.length; i++) {
-  // create a varaible smallestest positon intially set to first element.
-  let smallestPos = i; 
-  // Create a nest loop that iterates from the  i + 1. 
-    for(let j = i + 1; j < arr.length; j++) {
-      let current = arr[j];
-  // Check if current element is smalled than that smalles position element.
-      if(arr[smallestPos] > current) {
-        smallestPos = j; 
-      }
-    }
-  // check if i is not equal to the smallest position.
-    if(smallestPos !== i) {
-  // swap
-      let temp = arr[i];
-      arr[i] = arr[smallestPos];
-      arr[smallestPos] = temp;
-    }
-  }
-  // return the arr. 
-  return arr;
-}
+// // sort by moving the smallest value to front of on each pass.
+// function selectionSort(arr) {
+//   // create a for loop that iterates throgh the arr. 
+//   for(let i = 0; i < arr.length; i++) {
+//   // create a varaible smallestest positon intially set to first element.
+//   let smallestPos = i; 
+//   // Create a nest loop that iterates from the  i + 1. 
+//     for(let j = i + 1; j < arr.length; j++) {
+//       let current = arr[j];
+//   // Check if current element is smalled than that smalles position element.
+//       if(arr[smallestPos] > current) {
+//         smallestPos = j; 
+//       }
+//     }
+//   // check if i is not equal to the smallest position.
+//     if(smallestPos !== i) {
+//   // swap
+//       let temp = arr[i];
+//       arr[i] = arr[smallestPos];
+//       arr[smallestPos] = temp;
+//     }
+//   }
+//   // return the arr. 
+//   return arr;
+// }
 
-console.log(
-  "SELECTION ",
-  selectionSort([5, 3, 4, 1, 2]),//[1, 2, 3, 4, 5];
-  selectionSort([5, 5, 4, 1, 2]) //[1, 2, 4, 5, 5, 5];
-);
+// console.log(
+//   "SELECTION ",
+//   selectionSort([5, 3, 4, 1, 2]),//[1, 2, 3, 4, 5];
+//   selectionSort([5, 5, 4, 1, 2]) //[1, 2, 4, 5, 5, 5];
+// );
 
 // ---------------JAN 2.20 --------------------------------------------------------\\\
 
