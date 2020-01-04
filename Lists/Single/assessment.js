eb68ed99daf04a27d68d372955c4eb3b64ad5d09
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-04 00:59:24 -0500

    chore: add date

--- Lists/Single/assessment.js
+++ Lists/Single/assessment.js
@@ -1,51 +1,53 @@
-class SinglyLinkedList { 
-  constructor() { 
-    // Create a head.
-    this.head = null;
-    // Create   a tail.
-    this.tail = null;
-    // Set length to zero.  
-    this.length = 0; 
-  }
 
-  push(val) {
-    // store a new node with a val 
-    const node = new Node(val);
-    // check if the list has head
-    if(!this.head) { 
-    // if it doesnt set new node as head and tail 
-      this.head = node; 
-      this.tail = node; 
-    }
-    else {
-      // set this.tail.next to tail 
-      this.tail.next = node; 
-      this.tail = node; 
-    }
-    // update this.tail 
-    // increase length. 
-    this.length++; 
-  }
+// ---------------------------------------------jan.4.2020 --------------------------------------------------------------------------------------/////
 
-  pop() {
+// class SinglyLinkedList { 
+//   constructor() { 
+//     // Create a head.
+//     this.head = null;
+//     // Create   a tail.
+//     this.tail = null;
+//     // Set length to zero.  
+//     this.length = 0; 
+//   }
+
+//   push(val) {
+//     // store a new node with a val 
+//     const node = new Node(val);
+//     // check if the list has head
+//     if(!this.head) { 
+//     // if it doesnt set new node as head and tail 
+//       this.head = node; 
+//       this.tail = node; 
+//     }
+//     else {
+//       // set this.tail.next to tail 
+//       this.tail.next = node; 
+//       this.tail = node; 
+//     }
+//     // update this.tail 
+//     // increase length. 
+//     this.length++; 
+//   }
+
+//   pop() {
     
-  }
-}
+//   }
+// }
 
+// class Node { 
+//   constructor(val) {
+//     // set val.
+//     this.val = val; 
+//     // set next as null. 
+//     this.next = null; 
+//   }
+// }
 
-class Node { 
-  constructor(val) {
-    // set val.
-    this.val = val; 
-    // set next as null. 
-    this.next = null; 
-  }
-}
-
-const list = new SinglyLinkedList();
-list.push('ONE');
-list.push('TWO');
-console.log('LIST ', list);
+// const list = new SinglyLinkedList();
+// list.push('ONE');
+// list.push('TWO');
+// console.log('LIST ', list);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
