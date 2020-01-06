3b4657844765b95a10d6f4500c0a2546439608c1
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-05 20:26:14 -0500

    chore: redo digit count and getDigit functions

--- Sort/sortAlgosAssessment.js
+++ Sort/sortAlgosAssessment.js
@@ -1,135 +1,173 @@
 // ---------------JAN 3.20 --------------------------------------------------------\\\
 // // sort by moving the larger values to the end of the arr. 
 // function bubbleSort(arr) {
-
+//   // Create decrement  for loop. 
+//   for ( let i = arr.length; i > 0; i--) {
+//   // create a varaible noSwap set to true. 
+//     let noSwap = true; 
+//   // iterate through the array.
+//     for(let j = 0; j < i - 1 ; j++) {
+//   // create current variable.
+//       let current = arr[j];
+//   // create next varaible.
+//       let next = arr[j + 1];
+//   // check is the current element is greater than the next. 
+//       if(current > next) { 
+//   // swap.
+//         let temp = current; 
+//         arr[j] = next; 
+//         arr[j + 1] = temp; 
+//   // update noswap to false. 
+//         noSwap = false;
+//       }
+//     }
+//   // check if was nosap and break.
+//     if(noSwap) break;
+//   }
+//   // return arr. 
+//   return arr;
 // }
 
-// console.log('BUBBLE ', 
-//   bubbleSort(
-//   [5, 3, 4, 1, 2])// [1, 2, 3, 4, 5]
+// console.log(
+//   'BUBBLE ', 
+//   bubbleSort([5, 3, 4, 1, 2])// [1, 2, 3, 4, 5]
 // );
 
 //------Merge Sort-----//
 
-function merge(left, right) {
-  // Set an empty arr called result. 
-  const result = [];
-  // create a left idx pointer.
-  let leftIdx = 0;
-  // Create a right Idx pointer. 
-  let rightIdx = 0;
-  // Create a while loop comparing both lengths
-  while(leftIdx !== left.length && rightIdx !== right.length) {
-  // Check which arr has the smaller value 
-    if(left[leftIdx] < right[rightIdx]){
-  // push value to the result arr
-      result.push(left[leftIdx]);
-  // increase left 
-      leftIdx++;
-    }
-    else { 
-      result.push(right[rightIdx]);
-  // increase right
-      rightIdx++;
-    }
+// function merge(left, right) {
+//   // Set an empty arr called result. 
+//   const result = [];
+//   // create a left idx pointer.
+//   let leftIdx = 0;
+//   // Create a right Idx pointer. 
+//   let rightIdx = 0;
+//   // Create a while loop comparing both lengths
+//   while(leftIdx !== left.length && rightIdx !== right.length) {
+//   // Check which arr has the smaller value 
+//     if(left[leftIdx] < right[rightIdx]){
+//   // push value to the result arr
+//       result.push(left[leftIdx]);
+//   // increase left 
+//       leftIdx++;
+//     }
+//     else { 
+//       result.push(right[rightIdx]);
+//   // increase right
+//       rightIdx++;
+//     }
 
-  }
+//   }
 
-    // check from reamin values in left 
-    if(leftIdx !== left.length) {
-      let leftRemaining = left.slice(leftIdx);
-  // spread and push values to result
-      result.push(...leftRemaining);
-    }
+//     // check from reamin values in left 
+//     if(leftIdx !== left.length) {
+//       let leftRemaining = left.slice(leftIdx);
+//   // spread and push values to result
+//       result.push(...leftRemaining);
+//     }
 
-  // check for remaing values in right    
-    if(rightIdx !== right.length) {
-      const rightRemaining = right.slice(rightIdx);
-  // spread and push values to result
-      result.push(...rightRemaining);
-    }
-  // return result.
-  return result; 
-}
-
-function mergeSort(arr){
-  // The base is checking if the array length is less than or equal to 1. 
-  if(arr.length <= 1) return arr; 
-  // Find the midpoint 
-  const midpoint = Math.floor( arr.length / 2 );
-  // slice the lift side and pass it in megeSort again. 
-  let left = mergeSort(arr.slice(0, midpoint));
-  // slice the right side and pass it in mergeSort again. 
-  let right = mergeSort(arr.slice(midpoint));
-  // return the call of merge with both lift and right as arguments. 
-  return merge(left, right);
-}
-
-console.log(
-  'MERGE ',
-  mergeSort([10, 24, 76, 73, 72, 1, 9]),
-  mergeSort([10, 24, 76, 73])
-);
-
-//------Radix Sort-----//
-// // helper function to determine what number is in certina place. 
-// function getDigit(num, place) { 
-
+//   // check for remaing values in right    
+//     if(rightIdx !== right.length) {
+//       const rightRemaining = right.slice(rightIdx);
+//   // spread and push values to result
+//       result.push(...rightRemaining);
+//     }
+//   // return result.
+//   return result; 
 // }
 
-// // helper function to determine the length of a digit. 
-// function digitCount(num) { 
-
-// }
-
-// // helper function to determine the length of the longest digit in the arr.  
-// function mostDigits(arr) { 
-
-// }
-
-// // function to flatten the bucket 
-// function bucketFlattener(arr) { 
-
-// }
-
-// // sorting nimbers by place values.
-// function radixSort(arr) {
-
+// function mergeSort(arr){
+//   // The base is checking if the array length is less than or equal to 1. 
+//   if(arr.length <= 1) return arr; 
+//   // Find the midpoint 
+//   const midpoint = Math.floor( arr.length / 2 );
+//   // slice the lift side and pass it in megeSort again. 
+//   let left = mergeSort(arr.slice(0, midpoint));
+//   // slice the right side and pass it in mergeSort again. 
+//   let right = mergeSort(arr.slice(midpoint));
+//   // return the call of merge with both lift and right as arguments. 
+//   return merge(left, right);
 // }
 
 // console.log(
-//     "RADIX ",
-//   radixSort([215, 16, 31256]), // [16, 215, 31256]
-//   radixSort([10, 5, 7, 4, 3, 2, 1, 8, 6]), // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//   radixSort([10, 5, 7, 4, 3, 2, 1, 8, 6, 6]), // [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10]
-//   radixSort([23, 345, 5467, 12, 2345, 9852]) // [12, 23, 345, 2345, 5467, 9852]
+//   'MERGE ',
+//   mergeSort([10, 24, 76, 73, 72, 1, 9]),
+//   mergeSort([10, 24, 76, 73])
 // );
 
+//------Radix Sort-----//
+// helper function to determine what number is in certina place. 
+function getDigit(num, place) { 
+// find the mud of the abs value of the number divided by 10. 
+  return Math.floor(Math.abs(num) / Math.pow(10, place) % 10); 
+}
+
+// helper function to determine the length of a digit. 
+function digitCount(num) { 
+  // set length to 0. 
+  let length = 0; 
+  // set count to 1
+  let count = 1; 
+  // Create a while loop that checks if count 0s less than or eqaul to absolute value of num. 
+  while( count <= Math.abs(num)) {
+  // add to length.    
+    length++;
+  // mulitply count by 10. 
+    count *= 10; 
+  }
+  // return length. 
+  return length;
+}
+
+// helper function to determine the length of the longest digit in the arr.  
+function mostDigits(arr) { 
+
+}
+
+// function to flatten the bucket 
+function bucketFlattener(arr) { 
+
+}
+
+// sorting nimbers by place values.
+function radixSort(arr) {
+
+}
+
+console.log(
+    "RADIX ",
+  radixSort([215, 16, 31256]), // [16, 215, 31256]
+  radixSort([10, 5, 7, 4, 3, 2, 1, 8, 6]), // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  radixSort([10, 5, 7, 4, 3, 2, 1, 8, 6, 6]), // [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10]
+  radixSort([23, 345, 5467, 12, 2345, 9852]) // [12, 23, 345, 2345, 5467, 9852]
+);
+
 //------Selection Sort-----//
-// // sort by moving the smallest value to front of on each pass.
+// // // sort by moving the smallest value to front of on each pass.
 // function selectionSort(arr) {
-//   // create a for loop that iterates throgh the arr. 
+//   // iterate through the arr from the beginning. 
 //   for(let i = 0; i < arr.length; i++) {
-//   // create a varaible smallestest positon intially set to first element.
-//   let smallestPos = i; 
-//   // Create a nest loop that iterates from the  i + 1. 
+//   // Set smallestPos to i 
+//     let smallestPos = i; 
+//   // Create a for loop stating + 1 from i. 
 //     for(let j = i + 1; j < arr.length; j++) {
 //       let current = arr[j];
-//   // Check if current element is smalled than that smalles position element.
-//       if(arr[smallestPos] > current) {
+//   // check if the current element is smaller than smallestpos position. 
+//       if(current < arr[smallestPos]){
+//   // update smallestPos.
 //         smallestPos = j; 
 //       }
 //     }
-//   // check if i is not equal to the smallest position.
+//   // check if smallestPos is not the same as i
 //     if(smallestPos !== i) {
-//   // swap
+//   // Swap. 
 //       let temp = arr[i];
 //       arr[i] = arr[smallestPos];
-//       arr[smallestPos] = temp;
+//       arr[smallestPos] = temp; 
 //     }
 //   }
-//   // return the arr. 
-//   return arr;
+//   // return arr.
+//   return arr; 
 // }
 
 // console.log(
