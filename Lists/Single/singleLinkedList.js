9cfc330c9f710450173d08aa11c27a39d1521f3c
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-15 08:28:48 -0500

    chore: add psuedo code

--- Lists/Single/singleLinkedList.js
+++ Lists/Single/singleLinkedList.js
@@ -215,7 +215,12 @@ class SinglyLinkedList{
     // Swap the head and tail.
     // Create a variable called next.
     // Create a varaible called prev.
-    // Creat a variable called current 
+    // Creat a variable called current intialized to the head prop. 
+    // loop through the list. 
+    // Set next to be the next property on whatever node is. 
+    // Set the next property on the node to be whateve prev is. 
+    // Set the prev to be the value of the node variable. 
+    // Set the node variable to be the value of the next variable. 
   }
 }
 
