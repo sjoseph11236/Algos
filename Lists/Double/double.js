b23e7696b27cb1be92d2c3ac2ddfbea7db258cae
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-21 08:49:53 -0500

    chore: set up classes and create a list

--- Lists/Double/double.js
+++ Lists/Double/double.js
@@ -5,3 +5,24 @@
 
 
 // __________________________
+
+class Node { 
+  constructor(val) {
+    this.val = val;
+    this.next = null; 
+    this.prev = null; 
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null; 
+    this.tail = null; 
+    this.length = 0; 
+  }
+}
+
+
+const doublyLinkedList = new DoublyLinkedList();
+
+console.log('>>>> ', doublyLinkedList);
