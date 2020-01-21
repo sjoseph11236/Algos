3f55ab1606e187b93b817bc9bba12c648a6d5fb1
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-21 08:58:12 -0500

    feat: add push method

--- Lists/Double/double.js
+++ Lists/Double/double.js
@@ -20,6 +20,26 @@ class DoublyLinkedList {
     this.tail = null; 
     this.length = 0; 
   }
+
+  // Adding a node to the end of the Doubly Linked List
+  push(val) {
+    // Create a new node with the value passed in 
+    let newNode = new Node(val);
+    // Check if the head is null.
+    if(!this.head) {
+      // set the head and tail to the newNode.
+      this.head = newNode; 
+      this.tail = newNode;
+    }
+      // Set the next prop on the current tail to be the node 
+    this.tail.next = newNode; 
+    // Set the prev prop on the newly create node to be the tail. 
+    newNode.prv = this.tail; 
+    // update the tail to be the newNode. 
+    this.tail = newNode;
+    // Increment the length. 
+    this.length++;
+  }
 }
 
 
