35aa32053218da1bc17ad45cc40478c118900543
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-30 08:34:42 -0500

    feat: add stack assessment

---
+++ all_assessments/stack_assessment.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(val) {
+
+  }
+}
+
+class Stack { 
+  constructor() {
+
+  }
+
+  //Adding values to the top of the stack 
+  push(val) {
+
+
+  } 
+
+  pop(){
+
+  }
+}
+
+let stack = new Stack();
+
+// // PUSH
+// let callOne = stack.push('google');
+// console.log("The LENGTH is", callOne, callOne === 1);
+// console.log("TCL: callOne", stack.first.val , stack.first.val === 'google');
+// console.log("TCL: callOne", stack.last.val , stack.last.val === 'google');
+
+// let callTwo = stack.push('instagram');
+// console.log("The LENGTH is", callTwo, callTwo === 2);
+// console.log("TCL: callTwo", stack.first.val , stack.first.val === 'instagram');
+// console.log("TCL: callTwo ", stack.last.val , stack.last.val === 'google');
+
+// let callThree = stack.push('youtube');
+// console.log("The LENGTH is", callThree, callTwo === 2);
+// console.log("TCL: callTHREE", stack.first.val , stack.first.val === 'youtube');
+// console.log("TCL: callTHREE", stack.last.val , stack.last.val === 'google');
+
+
+// // POP
+// stack.push('google');
+// stack.push('instagram');
+// stack.push('youtube');
+
+// let callOne = stack.pop();
+// console.log("TCL: callOne ", callOne, callOne === 'youtube');
+// let callTwo = stack.pop();
+// console.log("TCL: callTwo ", callTwo , callTwo === 'instagram');
+// let callThree = stack.pop();
+// console.log("TCL: callThree", callThree, callThree === 'google');
+
+console.log('STACK ', stack);
