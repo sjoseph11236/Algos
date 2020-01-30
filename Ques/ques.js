264a6eb9a5b2e8ca79796f391dd3b66afd9aa4f6
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-30 08:52:14 -0500

    chore: add notes on ques

---
+++ Ques/ques.js
@@ -0,0 +1,23 @@
+// A que is a first in first out data structure. 
+
+
+// let que = [];
+
+// Adding values first in
+// que.push('FIRST');
+// que.push(' SECOND');
+// que.push('THIRD');
+
+// console.log('The que is ', que);
+
+// removing a values - first out
+// que.shift();
+// console.log('The que is ', que);
+
+class Node {
+  costructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
