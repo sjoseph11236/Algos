3ffb4975e0a4a66c6c0314e0533176f4803cb2e5
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-03 10:12:03 -0500

    feat: add countInversions

---
+++ Reacto/countInversions.js
@@ -0,0 +1,12 @@
+// Complete the function countInversions in the editor below. It must return an integer representing the number of inversions required to sort the array.
+
+function countInversions(arr) {
+  // check if length is less than 1.
+  // return arr. 
+  // Set the midpoint of the arr
+  // Sort the left 
+  // Sort the right
+  // return merge of left and right with helper function
+  }
+
+  console.log(countInversions([2,1,3,1,2]));
