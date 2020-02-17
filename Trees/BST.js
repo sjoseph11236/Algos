083be6ec412cc681947d0e385a23cb958b48da8f
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-17 10:16:36 -0500

    some notes on Trees

--- Trees/BST.js
+++ Trees/BST.js
@@ -12,34 +12,17 @@ class BinarySearchTree {
   }
   
   insert(value) {
-    // Create a new nodew with value from the parameter 
-    const newNode = new Node(value);
 
-    //Check if there is a root 
-    if(!this.root){
-      // set root to newNode
-      this.root = newNode;
-    }
-
-
-    // if(value > this.root.value) {
-    //   if(!this.right){
-    //     this.right = newNode;
-    //   }
-    //   else{ 
-
-    //   }
-    // }
   }
 }
 
 let tree = new BinarySearchTree();
-// tree.root = new Node(10);
-// tree.right  = new Node(15);
-// tree.left = new Node(7);
+tree.root = new Node(10);
+tree.right  = new Node(15);
+tree.left = new Node(7);
 
 // console.log('INSERT ', tree.insert(3));
 
 
 
-// console.log(tree);
+console.log(tree);
--- Trees/Trees.js
+++ Trees/Trees.js
@@ -1,3 +1,9 @@
 // TREES: 
 
 // A data structure that consists of nodes in parent and child relationship. 
+// Trees are nonlinear 
+// Root is top node in a tree 
+// A child directly connected to another node when movin away from the root. 
+// A sibilings are nodes with the same parent. 
+// A leaf is node with no children. 
+// Edge is the connection between one node and another.
