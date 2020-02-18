9e8edef4e53b4d0940666de82086335809609353
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-18 10:39:22 -0500

    chore: complete insert method

--- Trees/BST.js
+++ Trees/BST.js
@@ -12,15 +12,50 @@ class BinarySearchTree {
   }
   
   insert(value) {
-
+    // Create a newNode with value. 
+    const newNode = new Node(value);
+    // Check if there is a root. 
+    if(!this.root) { 
+      this.root = newNode;
+      return this;
+    }
+    // Set current to the root .
+    let current = this.root;
+    // Set  a while loop. 
+    while(true){
+    // check if value is less than current.value.      
+      if(value < current.value) { 
+    // check if current.left is null.
+        if(!current.left) { 
+    // Set newNode to current.left
+          current.left = newNode;
+    // return this
+          return this;
+        }
+    // Re-assign current to current.left.
+        current = current.left;
+      }
+    // check if values is greater than current.value. 
+      else if(value > current.value){
+        // check if if current.right is null      
+          if(!current.right) { 
+      // Set newNode to current.right.
+            current.right = newNode;
+      // return this
+            return this;
+          }
+      // Update current to current.right.
+          current = current.right;
+      }
+    }
   }
 }
 
 let tree = new BinarySearchTree();
 tree.root = new Node(10);
-tree.right  = new Node(15);
-tree.left = new Node(7);
-
+tree.insert(3);
+tree.insert(15);
+tree.insert(7);
 // console.log('INSERT ', tree.insert(3));
 
 
