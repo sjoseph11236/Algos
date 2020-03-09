803c5430691025df88cbee7863d2d6f58dbed101
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-09 09:54:05 -0400

    chore: add removeVertex function

--- Graphs/index.js
+++ Graphs/index.js
@@ -20,16 +20,52 @@ class Graph {
       if(edge !== vertex1) return edge;
     });
   }
+
+  removeVertex( vertex ){
+    while(this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  depthFristSeachRecursion(vertex){ 
+    // Accept a vertex.
+    // Create a list to store  visited
+    // Create am object to store visited verticies.
+    // create a helper function with acceps a vetes. 
+    // check if vertex is empty
+    // 
+  }
 }
 
 const First = new Graph();
-First.addVertex('Tokyo');
-First.addVertex('California');
-First.addVertex('New York');
-First.addEdge('New York', 'Tokyo');
-First.addEdge('New York', 'California');
-First.removeEdge('New York', 'California');
+// First.addVertex('Tokyo');
+// First.addVertex('California');
+// First.addVertex('New York');
+// First.addEdge('New York', 'Tokyo');
+// First.addEdge('New York', 'California');
+// First.removeEdge('New York', 'California');
+// First.removeVertex('New York');
+// console.log('FIRST ', First);
+
+First.addVertex('A');
+First.addVertex('B');
+First.addVertex('C');
+First.addVertex('D');
+First.addVertex('E');
+First.addVertex('F');
+
+First.addEdge('A','B');
+First.addEdge('A','C');
+First.addEdge('B','D');
+First.addEdge('C','E');
+First.addEdge('D','E');
+First.addEdge('D','F');
+First.addEdge('E','F');
+
 console.log('FIRST ', First);
+
 
 
 module.exports = {
